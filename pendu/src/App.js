e006ea59f8742f01f2c8d2c09fffe4900d8f7cf2
Najib Tahar <najibtaharberrabah@gmail.com>
2019-03-10 22:37:59 +0100

    generateWord

--- pendu/src/App.js
+++ pendu/src/App.js
@@ -3,20 +3,40 @@ import shuffle from 'lodash.shuffle'
 
 import './App.css';
 
-const words = ["Fleur", "Elephant", "Dinosaure"]
-console.log(shuffle(words))
+import Word from './word'
+
 class App extends Component {
 
+  state = {
+    word: this.generateWords(),
+    guesses: 0,
+  }
+  
+
+  generateWords() {
+    const word = []
+    const words = ["Fleur", "Elephant", "Dinosaure"]
+    const memo =  shuffle(words).pop().toLowerCase();
+    console.log(memo)
+    for (let i = 0; i < memo.length; i++) {
+      word.push(memo[i])
+    }
+    console.log(word);
+    return word
+  }
 
   
-  // generateWords() {
-  //   return shuffle(words)
-  // }
-    
+
   render() {
     return (
       <div className="App">
-
+        <h1>PENDU</h1>
+        <div className="word">
+          {this.state.word.map( letter =>(
+            // <div className="letter">{letter}</div>
+            <Word letter={letter}/>
+          ))}
+        </div>
       </div>
     );
   }
---
+++ pendu/src/word.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+const Word = ({letter}) => (
+   <div className="letter">{letter}</div>
+)
+
+export default Word
